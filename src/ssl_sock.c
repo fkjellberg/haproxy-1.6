rahulagarwal30/haproxy

BUG/MINOR: Use crt_base instead of ca_base when crt is parsed on a server line
In srv_parse_crt, crt_base was checked but ca_base was used to build the
certifacte path.

This patch must be backported in 1.7, 1.6 and 1.5.

src/ssl_sock.c
@@ -7714,7 +7714,7 @@ static int srv_parse_crt(char **args, int *cur_arg, struct proxy *px, struct ser
 	}
 
 	if ((*args[*cur_arg + 1] != '/') && global_ssl.crt_base)
-		memprintf(&newsrv->ssl_ctx.client_crt, "%s/%s", global_ssl.ca_base, args[*cur_arg + 1]);
+		memprintf(&newsrv->ssl_ctx.client_crt, "%s/%s", global_ssl.crt_base, args[*cur_arg + 1]);
 	else
 		memprintf(&newsrv->ssl_ctx.client_crt, "%s", args[*cur_arg + 1]);
 

fkjellberg/haproxy-1.6

(cherry picked from commit ff3a41eb3f990ee1cc25a5fa961fe8e490254d0c)
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit ec8a2b84a2fbb2d17e76a68f9853c0594d197227)

src/ssl_sock.c
@@ -4949,7 +4949,7 @@ static int srv_parse_crt(char **args, int *cur_arg, struct proxy *px, struct ser
 	}
 
 	if ((*args[*cur_arg + 1] != '/') && global.crt_base)
-		memprintf(&newsrv->ssl_ctx.client_crt, "%s/%s", global.ca_base, args[*cur_arg + 1]);
+		memprintf(&newsrv->ssl_ctx.client_crt, "%s/%s", global.crt_base, args[*cur_arg + 1]);
 	else
 		memprintf(&newsrv->ssl_ctx.client_crt, "%s", args[*cur_arg + 1]);
 
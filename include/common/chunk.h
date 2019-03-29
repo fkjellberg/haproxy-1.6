be5d21c1a7a8056906eb4de6d19d32d0ef6a0b17
Willy Tarreau <w@1wt.eu>
2019-03-29 18:13:36 +0100

    BUILD: chunk: properly declare pool_head_trash as extern
    This one was also declared without the extern modifier in an include
    file.
    
    This needs to be backported to 1.9.
    
    (cherry picked from commit 72d9f3351dcf154320add16431c60bac63e1106c)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit d1e653e5bb57accfa7d36f2fb0c5614c1a18f37b)
    [wla: backported for build issues, context edit]
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>
    (cherry picked from commit 4af70c69c18154b846654dbdc73351f51519230b)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 747063921be5e37212647bc1e279bfaebe3ce7e0)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- include/common/chunk.h
+++ include/common/chunk.h
@@ -36,7 +36,7 @@ struct chunk {
 	int len;	/* current size of the string from first to last char. <0 = uninit. */
 };
 
-struct pool_head *pool2_trash;
+extern struct pool_head *pool2_trash;
 
 /* function prototypes */
 

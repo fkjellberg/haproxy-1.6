haproxy/haproxy

BUG/MAJOR: config: verify that targets of track-sc and stick rules are present
Stick and track-sc rules may optionally designate a table in a different
proxy. In this case, a number of verifications are made such as validating
that this proxy actually exists. However, in multi-process mode, the target
table might indeed exist but not be bound to the set of processes the rules
will execute on. This will definitely result in a random behaviour especially
if these tables do require peer synchronization, because some tasks will be
started to try to synchronize form uninitialized areas.

The typical issue looks like this :

    peers my-peers
         peer foo ...

    listen proxy
         bind-process 1
         stick on src table ip
         ...

    backend ip
         bind-process 2
         stick-table type ip size 1k peers my-peers

While it appears obvious that the example above will not work, there are
less obvious situations, such as having bind-process in a defaults section
and having a larger set of processes for the referencing proxy than the
referenced one.

The present patch adds checks for such situations by verifying that all
processes from the referencing proxy are present on the other one in all
track-sc* and stick-* rules, and in sample fetch / converters referencing
another table so that sc_inc_gpc0() and similar are safe as well.

This fix must be backported to all maintained versions. It may potentially
disrupt configurations which already randomly crash. There hardly is any
intermediary solution though, such configurations need to be fixed.

src/cfgparse.c
@@ -2649,6 +2649,11 @@ int check_config_validity()
 					 curproxy->id, mrule->table.name ? mrule->table.name : curproxy->id);
 				cfgerr++;
 			}
+			else if (curproxy->bind_proc & ~target->bind_proc) {
+				ha_alert("Proxy '%s': stick-table '%s' referenced 'stick-store' rule not present on all processes covered by proxy '%s'.\n",
+				         curproxy->id, target->id, curproxy->id);
+				return 0;
+			}
 			else {
 				free((void *)mrule->table.name);
 				mrule->table.t = &(target->table);
@@ -2681,6 +2686,11 @@ int check_config_validity()
 				ha_alert("Proxy '%s': type of fetch not usable with type of stick-table '%s'.\n",
 					 curproxy->id, mrule->table.name ? mrule->table.name : curproxy->id);
 				cfgerr++;
+			}
+			else if (curproxy->bind_proc & ~target->bind_proc) {
+				ha_alert("Proxy '%s': stick-table '%s' referenced 'stick-store' rule not present on all processes covered by proxy '%s'.\n",
+				         curproxy->id, target->id, curproxy->id);
+				return 0;
 			}
 			else {
 				free((void *)mrule->table.name);

src/sample.c
@@ -1264,6 +1264,12 @@ int smp_resolve_args(struct proxy *p)
 				break;
 			}
 
+			if (p->bind_proc & ~px->bind_proc) {
+				ha_alert("parsing [%s:%d] : stick-table '%s' not present on all processes covered by proxy '%s'.\n",
+					 cur->file, cur->line, px->id, p->id);
+				return 0;
+			}
+
 			free(arg->data.str.area);
 			arg->data.str.area = NULL;
 			arg->unresolved = 0;

fkjellberg/haproxy-1.6

(cherry picked from commit 151e1ca989968f5092baa593efd9f485e4947d17)
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit c294490c4c977e9c1202ef76b2e0449a038caaa1)
(cherry picked from commit a7f9b5545e13a9222e4912fd746f61336e43806d)
[wt: no check_trk_action() in 1.7, checks are open-coded in cfgparse;
 s/ha_alert/Alert/;]
(cherry picked from commit 19cc81c4ac335bd195f508733ca1a9395cde67ac)
[wt: removed irrelevant parts; s/http_trk_idx/http_req_trk_idx ;
 warning, this is still bogus, see next commit]

src/cfgparse.c
@@ -7698,6 +7698,11 @@ int check_config_validity()
 				      curproxy->id, mrule->table.name ? mrule->table.name : curproxy->id);
 				cfgerr++;
 			}
+			else if (curproxy->bind_proc & ~target->bind_proc) {
+				Alert("Proxy '%s': stick-table '%s' referenced 'stick-store' rule not present on all processes covered by proxy '%s'.\n",
+				         curproxy->id, target->id, curproxy->id);
+				return 0;
+			}
 			else {
 				free((void *)mrule->table.name);
 				mrule->table.t = &(target->table);
@@ -7730,6 +7735,11 @@ int check_config_validity()
 				Alert("Proxy '%s': type of fetch not usable with type of stick-table '%s'.\n",
 				      curproxy->id, mrule->table.name ? mrule->table.name : curproxy->id);
 				cfgerr++;
+			}
+			else if (curproxy->bind_proc & ~target->bind_proc) {
+				Alert("Proxy '%s': stick-table '%s' referenced 'stick-store' rule not present on all processes covered by proxy '%s'.\n",
+				         curproxy->id, target->id, curproxy->id);
+				return 0;
 			}
 			else {
 				free((void *)mrule->table.name);
@@ -7766,6 +7776,11 @@ int check_config_validity()
 				      curproxy->id, trule->arg.trk_ctr.table.n ? trule->arg.trk_ctr.table.n : curproxy->id,
 				      tcp_trk_idx(trule->action));
 				cfgerr++;
+			}
+			else if (curproxy->bind_proc & ~target->bind_proc) {
+				Alert("Proxy '%s': stick-table '%s' referenced by the 'track-sc%d' rule not present on all processes covered by proxy '%s'.\n",
+				         curproxy->id, target->id, tcp_trk_idx(trule->action), curproxy->id);
+				return 0;
 			}
 			else {
 				free(trule->arg.trk_ctr.table.n);
@@ -7805,6 +7820,11 @@ int check_config_validity()
 				      curproxy->id, trule->arg.trk_ctr.table.n ? trule->arg.trk_ctr.table.n : curproxy->id,
 				      tcp_trk_idx(trule->action));
 				cfgerr++;
+			}
+			else if (curproxy->bind_proc & ~target->bind_proc) {
+				Alert("Proxy '%s': stick-table '%s' referenced by the 'track-sc%d' rule not present on all processes covered by proxy '%s'.\n",
+				         curproxy->id, target->id, tcp_trk_idx(trule->action), curproxy->id);
+				return 0;
 			}
 			else {
 				free(trule->arg.trk_ctr.table.n);
@@ -7870,6 +7890,11 @@ int check_config_validity()
 				      curproxy->id, hrqrule->arg.trk_ctr.table.n ? hrqrule->arg.trk_ctr.table.n : curproxy->id,
 				      http_req_trk_idx(hrqrule->action));
 				cfgerr++;
+			}
+			else if (curproxy->bind_proc & ~target->bind_proc) {
+				Alert("Proxy '%s': stick-table '%s' referenced by the 'track-sc%d' rule not present on all processes covered by proxy '%s'.\n",
+				         curproxy->id, target->id, http_req_trk_idx(hrqrule->action), curproxy->id);
+				return 0;
 			}
 			else {
 				free(hrqrule->arg.trk_ctr.table.n);

src/sample.c
@@ -1255,6 +1255,12 @@ int smp_resolve_args(struct proxy *p)
 				break;
 			}
 
+			if (p->bind_proc & ~px->bind_proc) {
+				Alert("parsing [%s:%d] : stick-table '%s' not present on all processes covered by proxy '%s'.\n",
+				      cur->file, cur->line, px->id, p->id);
+				return 0;
+			}
+
 			free(arg->data.str.str);
 			arg->data.str.str = NULL;
 			arg->unresolved = 0;
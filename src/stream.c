haproxy/haproxy

BUG/MEDIUM: stream: Don't forget to free s->unique_id in stream_free().
In stream_free(), free s->unique_id. We may still have one, because it's
allocated in log.c::strm_log() no matter what, even if it's a TCP connection
and thus it won't get free'd by http_end_txn().
Failure to do so leads to a memory leak.

This should probably be backported to all maintained branches.

src/stream.c
@@ -386,6 +386,8 @@ static void stream_free(struct stream *s)
 		offer_buffers(NULL, tasks_run_queue);
 	}
 
+	pool_free(pool_head_uniqueid, s->unique_id);
+
 	hlua_ctx_destroy(s->hlua);
 	s->hlua = NULL;
 	if (s->txn)

fkjellberg/haproxy-1.6

(cherry picked from commit 32211a17eb1f1a18d960ec2a451992a928aaaf95)
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit f49cc4bfbcd052e6dd448a564e3cd9505ae3fbae)
(cherry picked from commit 56fd8658819e504782e0580443ebcd351c5414c3)
[wt: context; s/pool_free/pool_free2; s/pool_head_uniqueid/pool2_uniqueid;]
(cherry picked from commit caab5497e4bb00cb689666625a36ff05c7113ba7)

src/stream.c
@@ -288,6 +288,8 @@ static void stream_free(struct stream *s)
 		offer_buffers(NULL, tasks_run_queue + applets_active_queue);
 	}
 
+	pool_free2(pool2_uniqueid, s->unique_id);
+
 	hlua_ctx_destroy(&s->hlua);
 	if (s->txn)
 		http_end_txn(s);
f3b7aee6b098608b38e62601c164b38bd5ee77d1
Joao Morais <jcmoraisjr@gmail.com>
2019-10-30 21:04:00 -0300

    BUG/MINOR: config: Update cookie domain warn to RFC6265
    The domain option of the cookie keyword allows to define which domain or
    domains should use the the cookie value of a cookie-based server
    affinity. If the domain does not start with a dot, the user agent should
    only use the cookie on hosts that matches the provided domains. If the
    configured domain starts with a dot, the user agent can use the cookie
    with any host ending with the configured domain.
    
    haproxy config parser helps the admin warning about a potentially buggy
    config: defining a domain without an embedded dot which does not start
    with a dot, which is forbidden by the RFC.
    
    The current condition to issue the warning implements RFC2109. This
    change updates the implementation to RFC6265 which allows domain without
    a leading dot.
    
    Should be backported to all supported versions. The feature exists at least
    since 1.5.
    
    (cherry picked from commit e1583751b67704f297060afaabe87fd7d8d602a2)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 72c48ce7f0e5a8811148c46b557032cbd4bb76d3)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit cea1b8a0ce12f50ea8f50f6094896f73de8bd34d)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    [ Cf: Patch applied in src/cfgparse.c ]
    (cherry picked from commit e33b598764dbd56bc90ad451254d7424c6e8e057)
    [wt: context adjustments]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit f59efdeddd628cc63863b34b3a98a66be70cd2c4)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/cfgparse.c
+++ src/cfgparse.c
@@ -3292,11 +3292,10 @@ int cfg_parse_listen(const char *file, int linenum, char **args, int kwm)
 					goto out;
 				}
 
-				if (*args[cur_arg + 1] != '.' || !strchr(args[cur_arg + 1] + 1, '.')) {
+				if (*args[cur_arg + 1] != '.') {
 					/* rfc2109, 4.3.2 Rejecting Cookies */
-					Warning("parsing [%s:%d]: domain '%s' contains no embedded"
-						" dots nor does not start with a dot."
-						" RFC forbids it, this configuration may not work properly.\n",
+					Warning("parsing [%s:%d]: domain '%s' contains no embedded dot,"
+						" this configuration may not work properly (see RFC6265#5.2.3).\n",
 						file, linenum, args[cur_arg + 1]);
 					err_code |= ERR_WARN;
 				}

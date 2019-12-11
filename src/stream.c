rahulagarwal30/haproxy

BUG/MEDIUM: stream: Be sure to release allocated captures for TCP streams
All TCP and HTTP captures are stored in 2 arrays, one for the request and
another for the response. In HAPRoxy 1.5, these arrays are part of the HTTP
transaction and thus are released during its cleanup. Because in this version,
the transaction is part of the stream (in 1.5, streams are still called
sessions), the cleanup is always performed, for HTTP and TCP streams.

In HAProxy 1.6, the HTTP transaction was moved out from the stream and is now
dynamically allocated only when required (becaues of an HTTP proxy or an HTTP
sample fetch). In addition, still in 1.6, the captures arrays were moved from
the HTTP transaction to the stream. This way, it is still possible to capture
elements from TCP rules for a full TCP stream. Unfortunately, the release is
still exclusively performed during the HTTP transaction cleanup. Thus, for a TCP
stream where the HTTP transaction is not required, the TCP captures, if any, are
never released.

Now, all captures are released when the stream is freed. This fixes the memory
leak for TCP streams. For streams with an HTTP transaction, the captures are now
released when the transaction is reset and not systematically during its
cleanup.

This patch must be backported as fas as 1.6.

src/stream.c
@@ -644,6 +644,18 @@ static void stream_free(struct stream *s)
 	flt_stream_release(s, 0);
 
 	if (fe) {
+		if (s->req_cap) {
+			struct cap_hdr *h;
+			for (h = fe->req_cap; h; h = h->next)
+				pool_free(h->pool, s->req_cap[h->index]);
+		}
+
+		if (s->res_cap) {
+			struct cap_hdr *h;
+			for (h = fe->rsp_cap; h; h = h->next)
+				pool_free(h->pool, s->res_cap[h->index]);
+		}
+
 		pool_free(fe->rsp_cap_pool, s->res_cap);
 		pool_free(fe->req_cap_pool, s->req_cap);
 	}

fkjellberg/haproxy-1.6

(cherry picked from commit 5939925a3805f9755cff5a3b9635c1a533bc9184)
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
[ Cf: changes in proto_http.c was moved from http_reset_txn() to
      http_end_txn_clean_session() because it is a better place to do some
      cleanup on the stream. But in 2.1, this last functions does not exist
      anymore. ]
(cherry picked from commit b6af6b3650a6a1209ba503b6937afef2c08402e8)
(cherry picked from commit 526c806c389539b529d9a8df24343bbc737495c7)
(cherry picked from commit 438cfba09b70e88dc3b6ec2ebb7232992d2bf94e)
[wt: context adjust; s/pool_free/pool_free2/]
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit b669c7403f6d31c1ddab14e6105f79866d23a887)

src/stream.c
@@ -313,6 +313,18 @@ static void stream_free(struct stream *s)
 	}
 
 	if (fe) {
+		if (s->req_cap) {
+			struct cap_hdr *h;
+			for (h = fe->req_cap; h; h = h->next)
+				pool_free2(h->pool, s->req_cap[h->index]);
+		}
+
+		if (s->res_cap) {
+			struct cap_hdr *h;
+			for (h = fe->rsp_cap; h; h = h->next)
+				pool_free2(h->pool, s->res_cap[h->index]);
+		}
+
 		pool_free2(fe->rsp_cap_pool, s->res_cap);
 		pool_free2(fe->req_cap_pool, s->req_cap);
 	}
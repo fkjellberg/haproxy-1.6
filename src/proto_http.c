0c3474b90ad55a60323cde82de68bf2336c650e0
Tim Duesterhus <tim@bastelstu.be>
2020-06-26 15:44:48 +0200

    BUG/MEDIUM: fetch: Fix hdr_ip misparsing IPv4 addresses due to missing NUL
    The IPv4 code did not take into account that the header value might not
    contain the trailing NUL byte, possibly reading stray data after the header
    value, failing the parse and testing the IPv6 branch. That one adds the
    missing NUL, but fails to parse IPv4 addresses.
    
    Fix this issue by always adding the trailing NUL.
    
    The bug was reported on GitHub as issue #715.
    
    It's not entirely clear when this bug started appearing, possibly earlier
    versions of smp_fetch_hdr guaranteed the NUL termination. However the
    addition of the NUL in the IPv6 case was added together with IPv6 support,
    hinting that at that point in time the NUL was not guaranteed.
    
    The commit that added IPv6 support was 69fa99292e689e355080d83ab19db4698b7c502b
    which first appeared in HAProxy 1.5. This patch should be backported to
    1.5+, taking into account the various buffer / chunk changes and the movement
    across different files.
    
    (cherry picked from commit 5cd00873f46f49f78587d4cc770e5ce7e979cc9c)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 1b752a347dc709520e5d1cd475b1ad783fe5897c)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 8ba978b2bcfb30ff7dfc3b3a98fd91fab1c49067)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 2ea0d4a1f05827fec0c3745db7f8c00c0c37a5ab)
    [wt: applied by hand to proto_http.c; s/buffer/chunk/]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 924bcd5588084e96f432ce350405e5d47781c3c9)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 22a68146be7d06015acb81f21b9964a71ba0e9ed)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/proto_http.c
+++ src/proto_http.c
@@ -10794,20 +10794,19 @@ static int
 smp_fetch_hdr_ip(const struct arg *args, struct sample *smp, const char *kw, void *private)
 {
 	int ret;
+	struct chunk *temp = get_trash_chunk();
 
 	while ((ret = smp_fetch_hdr(args, smp, kw, private)) > 0) {
-		if (url2ipv4((char *)smp->data.u.str.str, &smp->data.u.ipv4)) {
-			smp->data.type = SMP_T_IPV4;
-			break;
-		} else {
-			struct chunk *temp = get_trash_chunk();
-			if (smp->data.u.str.len < temp->size - 1) {
-				memcpy(temp->str, smp->data.u.str.str, smp->data.u.str.len);
-				temp->str[smp->data.u.str.len] = '\0';
-				if (inet_pton(AF_INET6, temp->str, &smp->data.u.ipv6)) {
-					smp->data.type = SMP_T_IPV6;
-					break;
-				}
+		if (smp->data.u.str.len < temp->size - 1) {
+			memcpy(temp->str, smp->data.u.str.str,
+			       smp->data.u.str.len);
+			temp->str[smp->data.u.str.len] = '\0';
+			if (url2ipv4((char *) temp->str, &smp->data.u.ipv4)) {
+				smp->data.type = SMP_T_IPV4;
+				break;
+			} else if (inet_pton(AF_INET6, temp->str, &smp->data.u.ipv6)) {
+				smp->data.type = SMP_T_IPV6;
+				break;
 			}
 		}
 

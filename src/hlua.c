haproxy/haproxy

BUG/MINOR: lua: Check argument type to convert it to IP mask in arg validation
In hlua_lua2arg_check() function, before converting an argument to an IPv4 or
IPv6 mask, we must be sure to have an integer or a string argument (ARGT_SINT or
ARGT_STR).

This patch must be backported to all supported versions.

src/hlua.c
@@ -752,11 +752,17 @@ __LJMP int hlua_lua2arg_check(lua_State *L, int first, struct arg *argp,
 			break;
 
 		case ARGT_MSK4:
-			memcpy(trash.area, argp[idx].data.str.area,
-			       argp[idx].data.str.data);
-			trash.area[argp[idx].data.str.data] = 0;
-			if (!str2mask(trash.area, &argp[idx].data.ipv4))
-				WILL_LJMP(luaL_argerror(L, first + idx, "invalid IPv4 mask"));
+			if (argp[idx].type == ARGT_SINT)
+				len2mask4(argp[idx].data.sint, &argp[idx].data.ipv4);
+			else if (argp[idx].type == ARGT_STR) {
+				memcpy(trash.area, argp[idx].data.str.area,
+				       argp[idx].data.str.data);
+				trash.area[argp[idx].data.str.data] = 0;
+				if (!str2mask(trash.area, &argp[idx].data.ipv4))
+					WILL_LJMP(luaL_argerror(L, first + idx, "invalid IPv4 mask"));
+			}
+			else
+				WILL_LJMP(luaL_argerror(L, first + idx, "integer or string expected"));
 			argp[idx].type = ARGT_MSK4;
 			break;
 
@@ -772,11 +778,17 @@ __LJMP int hlua_lua2arg_check(lua_State *L, int first, struct arg *argp,
 			break;
 
 		case ARGT_MSK6:
-			memcpy(trash.area, argp[idx].data.str.area,
-			       argp[idx].data.str.data);
-			trash.area[argp[idx].data.str.data] = 0;
-			if (!str2mask6(trash.area, &argp[idx].data.ipv6))
-				WILL_LJMP(luaL_argerror(L, first + idx, "invalid IPv6 mask"));
+			if (argp[idx].type == ARGT_SINT)
+				len2mask6(argp[idx].data.sint, &argp[idx].data.ipv6);
+			else if (argp[idx].type == ARGT_STR) {
+				memcpy(trash.area, argp[idx].data.str.area,
+				       argp[idx].data.str.data);
+				trash.area[argp[idx].data.str.data] = 0;
+				if (!str2mask6(trash.area, &argp[idx].data.ipv6))
+					WILL_LJMP(luaL_argerror(L, first + idx, "invalid IPv6 mask"));
+			}
+			else
+				WILL_LJMP(luaL_argerror(L, first + idx, "integer or string expected"));
 			argp[idx].type = ARGT_MSK6;
 			break;
 

fkjellberg/haproxy-1.6

(cherry picked from commit e663a6e326c3d4d465d5aa66e4fbe3acde69aac3)
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit 2ef95fa486eae674cb32c491c7f5ca5da3299fee)
(cherry picked from commit 1f02e529ad337243931edb33ce6777b6f3a1c3a7)
(cherry picked from commit ff8de2c50b167e3a7827adec869a08fa03cc1743)
[wt: adjusted context; dropped changes to IPv6 mask which was not in 1.8]
(cherry picked from commit f60ba9ca88a25680ca02d7570423f43df2b64f36)
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
(cherry picked from commit 098526469c45cc3ba195abf453ec8134601ec989)

src/hlua.c
@@ -794,10 +794,16 @@ __LJMP int hlua_lua2arg_check(lua_State *L, int first, struct arg *argp,
 			break;
 
 		case ARGT_MSK4:
-			memcpy(trash.str, argp[idx].data.str.str, argp[idx].data.str.len);
-			trash.str[argp[idx].data.str.len] = 0;
-			if (!str2mask(trash.str, &argp[idx].data.ipv4))
-				WILL_LJMP(luaL_argerror(L, first + idx, "invalid IPv4 mask"));
+			if (argp[idx].type == ARGT_SINT)
+				len2mask4(argp[idx].data.sint, &argp[idx].data.ipv4);
+			else if (argp[idx].type == ARGT_STR) {
+				memcpy(trash.str, argp[idx].data.str.str, argp[idx].data.str.len);
+				trash.str[argp[idx].data.str.len] = 0;
+				if (!str2mask(trash.str, &argp[idx].data.ipv4))
+					WILL_LJMP(luaL_argerror(L, first + idx, "invalid IPv4 mask"));
+			}
+			else
+				WILL_LJMP(luaL_argerror(L, first + idx, "integer or string expected"));
 			argp[idx].type = ARGT_MSK4;
 			break;
 